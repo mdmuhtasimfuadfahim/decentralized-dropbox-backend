4992b056cb0c273d412e06b5f10eae6fb270ea11
Muhtasim Fahim <muhtasim.fahim.98@gmail.com>
2022-08-20 22:11:02 +0600

    catchAsync modified to require

--- src/controllers/ipfs.controller.js
+++ src/controllers/ipfs.controller.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const catchAsync = require('../utils/catchAsync');
+const catchAsync = require('../utils/CatchAsync');
 const { ipfsService, uploadService } = require('../services');
 const { success, error } = require('../utils/ApiResponse');
 
